06da718a950ec89695f92ad993163329753304f6
XueyingChen <115363697+Dkaattae@users.noreply.github.com>
2025-04-15 16:31:14 -0500

    Update get_stock_price.py

--- get_stock_price.py
+++ get_stock_price.py
@@ -1,12 +1,13 @@
 import yfinance as yf
 from datetime import datetime, timedelta
 import json
+import sys
+import csv
 
 def get_stock_price(txn):
 	ticker = txn['ticker']
 	date_str = txn['date']
 	txn_type = txn['type']
-	shares = txn['shares']
 	date = datetime.strptime(date_str, '%Y-%m-%d')
 	start_date = (date - timedelta(days=2)).strftime('%Y-%m-%d')
 	end_date = (date + timedelta(days=2)).strftime('%Y-%m-%d')
@@ -18,22 +19,40 @@ def get_stock_price(txn):
 		print(f"No data found for {ticker} around {date_str}")
 
 	closing_price = data.loc[date_str]['Close'].values[0].item()
-	stock_value = closing_price * shares
-	if txn_type.lower() == 's':
-		stock_value *= -1
-	cash_value = -stock_value
+	high_price = data.loc[date_str]['High'].values[0].item()
+	low_price = data.loc[date_str]['Low'].values[0].item()
+	# stock_value = closing_price * trading_shares
+	# if txn_type.lower() == 's':
+	# 	stock_value *= -1
+	# cash_value = -stock_value
 
 	result = {
 		'ticker': ticker,
 		'date': date_str,
 		'type': txn_type,
-		'shares': shares,
 		'closing_price': round(closing_price, 2),
-		'stock_value': round(stock_value, 2),
-		'cash_value': round(cash_value, 2)
+		'high_price': round(high_price, 2),
+		'low_price': round(low_price,2)
 	}
 	return result
 
 
-
-
+if __name__ == "__main__":
+    input_file = sys.argv[1]  # Read from command-line args
+    stock_data = []
+    error_log = []
+    with open(input_file, 'r') as stock_lists:
+        csv_reader = csv.DictReader(stock_lists)
+        for txn in csv_reader:
+            try:
+                stock_data.append(get_stock_price(txn))
+            except Exception as e:
+                error_log.append(["ticker": txn['ticker'], "date": txn['date'], "error": e])
+    file_path = 'stock_data.csv'
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(stock_data)
+    with open('stock_data_error_log.csv', 'w', newline = '') as efile:
+    	writer = csv.write(efile)
+    	writer.writerow(error_log)
+    
